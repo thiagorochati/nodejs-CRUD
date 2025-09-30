a3ea2e7bb41b465dfdf3c7ab9982a942e659c0a9
thiagorochati <thiagorochati@gmail.com>
2025-09-30 19:51:32 -0300

    30/09/2025
    Modal no momento da exclusão

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize("escola", "root", "root", {
+const connection = new Sequelize("escola", "root", "", {
   host: "localhost",
   dialect: "mysql"
 });
